4a20d9e1cf0f5ba7900b6ea06909467ca43ffacc
dringkbar <nagito12396@gmail.com>
2022-04-21 18:09:54 +0900

    fix iterator

--- src/iterator/Iterator.hpp
+++ src/iterator/Iterator.hpp
@@ -71,14 +71,14 @@ namespace ft
 	};
 
 	template <typename T>
-	class random_access_iterator : public ft::iterator<ft::random_access_iterator_tag, T>
+	class random_access_iterator
 	{
 	public:
-		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::value_type value_type;
-		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type difference_type;
-		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::pointer pointer;
-		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::reference reference;
-		typedef typename ft::iterator<ft::random_access_iterator_tag, T>::iterator_category iterator_category;
+		typedef typename ft::iterator_traits<T>::value_type value_type;
+		typedef typename ft::iterator_traits<T>::difference_type difference_type;
+		typedef typename ft::iterator_traits<T>::pointer pointer;
+		typedef typename ft::iterator_traits<T>::reference reference;
+		typedef typename ft::iterator_traits<T>::iterator_category iterator_category;
 
 	private:
 		T *_p;
